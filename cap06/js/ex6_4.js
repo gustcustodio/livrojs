3a3c24436dee0e438b64a3133462d585f44934ab
gustcustodio <gustcustodio@gmail.com>
2024-07-24 06:56:03 -0300

    Update ex6_4.js

--- cap06/js/ex6_4.js
+++ cap06/js/ex6_4.js
@@ -8,10 +8,10 @@ frm.addEventListener("submit", (e) => {
 
   const nome = frm.inNome.value;
   const idade = Number(frm.inIdade.value);
-  criancas.push({ nome, idade });
+  criancas.push({ nome, idade }); // adiciona dados o vetor de objetos
   frm.reset();
   frm.inNome.focus();
-  frm.btListar.dispatchEvent(new Event("click"));
+  frm.btListar.dispatchEvent(new Event("click")); // dispara click em btListar
 });
 
 frm.btListar.addEventListener("click", () => {
@@ -21,8 +21,8 @@ frm.btListar.addEventListener("click", () => {
   }
 
   let lista = "";
-  for (const crianca of criancas) {
-    const { nome, idade } = crianca;
+  for (const crianca of criancas) { // percorre o array crianças
+    const { nome, idade } = crianca; // desestrutura o objeto criança
     lista += nome + " - " + idade + " anos\n";
   }
   resp.innerText = lista;
@@ -34,15 +34,17 @@ frm.btResumir.addEventListener("click", () => {
     return;
   }
 
-  const copia = [...criancas];
-  copia.sort((a, b) => a.idade - b.idade); // função de comparação
+  const copia = [...criancas]; // cria um cópia do array de crinças (operador Rest)
+  // ordena a cópia do array por idade usando o método sort e função de comparação:
+  copia.sort((a, b) => a.idade - b.idade); 
 
   let resumo = "";
+  // define a idade auxiliar como a idade da primeira criança: 
   let aux = copia[0].idade;
   let nomes = [];
 
-  for (const crianca of copia) {
-    const { nome, idade } = crianca;
+  for (const crianca of copia) { // percorre a cópia do array de crianças
+    const { nome, idade } = crianca; // desestrutura o objeto criança
 
     if (idade == aux) {
       nomes.push(nome);
