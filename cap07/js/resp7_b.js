6700e1b77d3f247015cda6e244fd54da366e0e05
gustcustodio <gustcustodio@gmail.com>
2024-08-06 06:55:53 -0300

    cap7 - resp b, c

--- cap07/js/resp7_b.js
+++ cap07/js/resp7_b.js
@@ -2,17 +2,31 @@ const frm = document.querySelector("form");
 const resp = document.querySelector("h3");
 
 frm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const frase = frm.inFrase.value.toUpperCase();
-    const fraseInvertida = frase.split('').reverse().join('').toUpperCase();
-    let resposta = "";
+  const frase = frm.inFrase.value.toUpperCase();
+  const fraseNoSpace = frase.replace(/ /g, "");
+  const fraseInvertida = fraseNoSpace.split("").reverse().join("");
 
-    if (frase == fraseInvertida) {
-        resposta = `${frase} é um Palíndromo`;
-    } else {
-        resposta = "Não é um Palíndromo";
-    }
+  let resposta = "";
 
-    resp.innerText = resposta;
-})
+  if (frase.indexOf(" ") == -1) {
+    alert("Informe uma frase!");
+    frm.inFrase.focus();
+    return;
+  }
+
+  if (fraseNoSpace == fraseInvertida) {
+    resposta = `${frase} é um Palíndromo`;
+  } else {
+    resposta = "Não é um Palíndromo";
+  }
+
+  /* 
+  console.log(frase);
+  console.log(fraseNoSpace);
+  console.log(fraseInvertida); 
+  */
+
+  resp.innerText = resposta;
+});
---
+++ cap07/js/resp7_c.js
@@ -0,0 +1,31 @@
+const frm = document.querySelector("form");
+const resp1 = document.querySelector("#outResp1");
+const resp2 = document.querySelector("#outResp2");
+
+frm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const dataInfracao = frm.inData.value;
+    const valor = Number(frm.inValor.value);
+    const hoje = new Date();
+
+    console.log(dataInfracao);
+    console.log(hoje);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Suponha que o prazo para o pagamento de uma infração de trânsito com desconto
+seja de 90 dias. Elaborar um programa que leia a data de uma infração e o valor
+da multa. Informe qual a data limite do pagamento, com desconto (até 90 dias) e o
+valor a ser pago até essa data (com 20% de desconto) */
