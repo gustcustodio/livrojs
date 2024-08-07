3e519295f46c36c62e158387195006364bb9e30a
gustcustodio <gustcustodio@gmail.com>
2024-08-07 06:49:58 -0300

    Update resp7_c.js

--- cap07/js/resp7_c.js
+++ cap07/js/resp7_c.js
@@ -3,27 +3,17 @@ const resp1 = document.querySelector("#outResp1");
 const resp2 = document.querySelector("#outResp2");
 
 frm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const dataInfracao = frm.inData.value;
-    const valor = Number(frm.inValor.value);
-    const hoje = new Date();
+  const dataInfracao = frm.inData.value;
+  const valor = Number(frm.inValor.value);
+  const valorComDesconto = valor * 0.8;
+  const dataLimite = new Date();
 
-    console.log(dataInfracao);
-    console.log(hoje);
-})
+  resp2.innerText = `Valor com desconto R$${valorComDesconto.toFixed(2)}`;
 
-
-
-
-
-
-
-
-
-
-
-
+  console.log(dataInfracao);
+});
 
 /* Suponha que o prazo para o pagamento de uma infração de trânsito com desconto
 seja de 90 dias. Elaborar um programa que leia a data de uma infração e o valor
