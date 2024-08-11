041f5e1a24041f8a4be6eb8b93911a90b1e31616
gustcustodio <gustcustodio@gmail.com>
2024-08-11 12:15:14 -0300

    att

--- cap08/js/resp8_a.js
+++ cap08/js/resp8_a.js
@@ -1,12 +1,26 @@
 const frm = document.querySelector("form");
 const resp = document.querySelector("pre");
 
+function retornarTracos(nome) {
+  let tracos = "";
+  nome.split(" ");
+
+  for (const n of nome) {
+    if (n == " ") {
+      tracos += " ";
+    } else {
+      tracos += "-".repeat(n.length);
+    }
+  }
+  return tracos;
+}
+
 function categorizarAluno(idade) {
   let categoria = "";
 
   if (idade <= 12) {
     categoria = "Infantil";
-  } else if (idade >= 13 && idade <= 18) {
+  } else if (idade <= 18) {
     categoria = "Juvenil";
   } else {
     categoria = "Adulto";
@@ -19,6 +33,8 @@ frm.addEventListener("submit", (e) => {
 
   const nome = frm.inNome.value;
   const idade = Number(frm.inIdade.value);
+  const tracos = retornarTracos(nome);
+  const categoria = categorizarAluno(idade);
 
-  resp.innerText = `Categoria: ${categorizarAluno(idade)}`;
+  resp.innerText = nome + "\n" + tracos + "\nCategoria: " + categoria;
 });
