30724bdb3734eb597473642f2dd985457407c1fb
gustcustodio <gustcustodio@gmail.com>
2024-08-11 17:38:28 -0300

    cap8 - exercício C

--- cap08/js/resp8_c.js
+++ cap08/js/resp8_c.js
@@ -1,12 +1,42 @@
 const frm = document.querySelector("form");
-const convenio = document.querySelector("#selectConvenio")
+const tipoConvenio = document.querySelector("#selectConvenio");
 const resp1 = document.querySelector("#outResp1");
 const resp2 = document.querySelector("#outResp2");
 
-frm.rbSim.addEventListener("click", () => {
-    selectConvenio.className = "exibe";
-})
+// função que calcula o desconto com base no valor e percentual fornecido:
+function calcularDesconto(valor, desc) {
+  let desconto = valor * desc;
+  return desconto;
+}
 
+// evento ao clicar na opção "Sim" para convênio:
+frm.rbSim.addEventListener("click", () => {
+  selectConvenio.className = "exibe"; // exibe o campo de seleção de convênios
+});
+
+// evento ao clicar na opção "Não" para convênio:
 frm.rbNao.addEventListener("click", () => {
-    selectConvenio.className = "oculta";
-})
+  selectConvenio.className = "oculta"; // oculta o campo de seleção de convênios
+});
+
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const valor = Number(frm.inValor.value);
+
+  let desconto;
+  // verifica se o convênio foi selecionado:
+  if (frm.rbSim.checked) {
+    // verifica se o convênio é "saúde animal":
+    if (frm.selConvenio.value == "saudeAnimal") {
+      desconto = calcularDesconto(valor, 0.5); // aplica um desconto de 50%
+    } else { // se o convênio for outro
+      desconto = calcularDesconto(valor, 0.2); // aplica um desconto de 20%
+    }
+  } else { // se o convênio não foi selecionado
+    desconto = calcularDesconto(valor, 0.1); // aplica um desconto padrão de 10%
+  }
+
+  resp1.innerText = `Desconto R$${desconto.toFixed(2)}`;
+  resp2.innerText = `A pagar R$${(valor - desconto).toFixed(2)}`;
+});
