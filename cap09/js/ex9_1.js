c4cc778a318c80054242b328c09e09ae400d61fe
gustcustodio <gustcustodio@gmail.com>
2024-10-10 07:02:20 -0300

    Create ex9_1.js

---
+++ cap09/js/ex9_1.js
@@ -0,0 +1,7 @@
+const frm = document.querySelector("form");
+const imgClube = document.querySelector("#imgClube");
+const divTitulo = document.querySelector("#divTitulo");
+
+const trocarClube = () => {
+  let clube;
+};
